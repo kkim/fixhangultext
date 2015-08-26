6f6e28b5ef473c94133a87b54f99624cbddf74b7
Kio Kim SB <kiokim@gmail.com>
2015-08-25 22:25:28 -0700

    removed should_begin subrule: many space

--- paragraph.py
+++ paragraph.py
@@ -34,8 +34,6 @@ def should_continue(s):
 def should_begin(s):
     ''' Returns true if a line starts from here.
 
-    >>> should_begin(u'   여행의 시작')
-    True
     >>> should_begin(u'3. 여행의 시작')
     True
     >>> should_begin(u'- 여행의 시작')
@@ -43,7 +41,6 @@ def should_begin(s):
     >>> should_begin(u'여행의 시작')
     False
     '''
-    if s.startswith('   '): return True
     if s.startswith('- '): return True
     if re.match('^\d+\.',s.strip()): return True
     return False
